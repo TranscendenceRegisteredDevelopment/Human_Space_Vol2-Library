085483d8c06f8719aa2ad806563d10dc1c927719
Arisaya <117544545+ArisayaDragon@users.noreply.github.com>
2025-05-06 02:18:21 -0700

    chore: add build scripts, set an appropriate starting version, add .gitignore

---
+++ makeHS2.bat
@@ -0,0 +1,11 @@
+@ECHO off
+DEL _IMPORT\*.tdb
+DEL _TDB\*.tdb
+REN _Transcendence.tdb Transcendence.tdb
+IF NOT EXIST _TDB (MD _TDB)
+ECHO "========================================================================"
+ECHO "===========================     Lib Vol2   ============================="
+ECHO "========================================================================"
+TransCompiler.exe /input:HS2_LibCore.xml /output:_TDB/HS2_LibVol1.tdb
+REN Transcendence.tdb _Transcendence.tdb
+@ECHO on

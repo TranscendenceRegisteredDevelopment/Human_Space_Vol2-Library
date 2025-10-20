5c91a5fd96955263e46671abad31f4ef9512fffa
Arisaya <117544545+ArisayaDragon@users.noreply.github.com>
2025-10-20 12:10:13 -0700

    feat: add ares ship graphics, add base hammerheads for COHS, add templates

--- makeHS2.bat
+++ makeHS2.bat
@@ -6,6 +6,6 @@ IF NOT EXIST _TDB (MD _TDB)
 ECHO "========================================================================"
 ECHO "===========================     Lib Vol2   ============================="
 ECHO "========================================================================"
-TransCompiler.exe /input:HS2_LibCore.xml /output:_TDB/HS2_LibVol1.tdb
+TransCompiler.exe /input:HS2_LibCore.xml /output:_TDB/HS2_LibCore.tdb
 REN Transcendence.tdb _Transcendence.tdb
 @ECHO on
